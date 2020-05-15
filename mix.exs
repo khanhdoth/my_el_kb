ae81016a8b1fc9bb226804f87665709264245f02
khanhdoth <khanh.doth@gmail.com>
2020-05-15 20:38:52 +0000

    u

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MyElKb.MixProject do
     [
       app: :my_el_kb,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -20,7 +20,7 @@ defmodule MyElKb.MixProject do
   def application do
     [
       mod: {MyElKb.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :peerage]
     ]
   end
 
@@ -34,17 +34,19 @@ defmodule MyElKb.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.1"},
+      {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      # {:phoenix_live_dashboard, "~> 0.2.0"},
+      # {:telemetry_metrics, "~> 0.4"},
+      # {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:peerage, "~> 1.0"}
     ]
   end
 
